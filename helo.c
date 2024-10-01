24ff46739d0b1d12dae937bdeacbd1eac254ea51
aayush rana magar <realnaramagaraayush@gmail.com>
2024-10-01 20:57:26 +0545

    intital commit

---
+++ helo.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<Windows.h>
+
+#define charLimit 20
+
+void breker(char inputString[],char resultString[],int num){
+    printf("%s \n %s \n %d",inputString,resultString,num);
+}
+
+int main(){
+
+    char inputString[charLimit], resultString[charLimit];
+    int i, stringLength;
+
+    scanf("%[^\n]s",&inputString);
+    stringLength = strlen(inputString);
+
+    //intitlize the result tring as blanks or ' ' 
+
+    for(i=0;i<charLimit;i++){
+        resultString[i]=' ';
+    }
+
+    for(i=0;i<stringLength;i++){
+        resultString[i]='a';
+        while(resultString[i] != inputString[i]){
+            printf("%s\n",resultString);
+            resultString[i]++;
+            Sleep(10);
+        }
+    }
+
+     printf("%s\n",resultString);
+
+    getch();
+    return 0;
+}
